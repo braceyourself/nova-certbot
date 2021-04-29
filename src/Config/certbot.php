567d791683395817e5ac3228cde9a3bc513ae662
Ethan Brace <ethanabrace@gmail.com>
2021-04-29 06:34:01 -0400

    update config variable

--- src/Config/certbot.php
+++ src/Config/certbot.php
@@ -3,7 +3,8 @@
 return [
     'server' => '10.2.1.1',
     'server_user' => 'infotech',
-    'server_key' => storage_path('malachi.key'),
+    'server_key' => keys_path('
+    alachi.key'),
 
     'use_staging' => true,
     'logger_interface' => null,
